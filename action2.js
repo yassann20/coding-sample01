60d7108d7bc08ff9f41154b7646b5576b08c4991
yassann20 <kaiseif4e@gmail.com>
2024-07-10 04:21:51 +0900

    ccontent1 フェードイン修正

--- action2.js
+++ action2.js
@@ -1,18 +1,19 @@
-$('.question1').on('inview', function(event, isInView){
-    if(!$(this).stop().hasClass('slide-in-l') && isInView){
-        $(this).stop().addClass('slide-in-l');
-    }
-});
-$('.question2').on('inview', function(event, isInView){
-    if(!$(this).stop().hasClass('slide-in-r') && isInView){
-        $(this).stop().addClass('slide-in-r');
-    }
-});
-$('.growing').on('inview', function(event, isInView){
-    if(isInView){
-        $(this).stop().addClass('growing-in');
-    }
-});
-$('.hamburger').on('click', function(){
-    $("nav, span").stop().toggleClass("active");
+$(function () {
+    $('.hamburger').on('click', function () {
+        $("nav, span").stop().toggleClass("active");
+    });
+    $('.question-l').on('inview', function () {
+
+        $('.question-l').stop().removeClass('hidden');
+    });
+    $('.question-r').on('inview', function () {
+
+        $('.question-r').stop().removeClass('hidden');
+    });
+    $('.growing').on('inview', function (event, isInView) {
+        if (isInView) {
+            $(this).stop().addClass('growing-in');
+        }
+    });
+
 });
